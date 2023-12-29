8f25bd0967d117cdbcec32ba9883f54707d14535
cswansonbooth125 <cswansonbooth125@gmail.com>
2023-12-29 04:42:25 -0700

    Made webpage a php file and added display_table.php to file

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="main-header">
+        <img src="images/CTHS-logo.png" alt="CTHS-logo" style="width: 10%;">
+        <h1>Our Partners</h1>
+    </div>
+    
+    <?php
+        include("display_table.php");
+    ?>
+
+</body>
+</html>
