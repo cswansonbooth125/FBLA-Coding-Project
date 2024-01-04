a61377a0618bcff8d281a81e91eb3e95fcfe4b98
cswansonbooth125 <cswansonbooth125@gmail.com>
2024-01-03 23:17:49 -0700

    reformatted if and while statements

--- display-table.php
+++ display-table.php
@@ -8,24 +8,29 @@
     $conn = new mysqli($servername, $username, $password, $dbname);
 
     // Check connection
-    if ($conn->connect_error) {
+    if ($conn->connect_error) 
+    {
         die("Connection failed: " . $conn->connect_error);
     }
 
     $sql = "SELECT * FROM partners_table";
     $result = $conn->query($sql);
 
-    if ($result->num_rows > 0) {
+    if ($result->num_rows > 0) 
+    {
         // Fetch data and display it in HTML table
         echo "<table class='info-table'>";
         echo "<tr><th>Name</th><th>Type</th><th>Resources</th><th>Contact_info</th></tr>";
 
-        while($row = $result->fetch_assoc()) {
+        while($row = $result->fetch_assoc()) 
+        {
             echo "<tr><td>" . $row["Name"] . "</td><td>" . $row["Type"] . "</td><td>" . $row["Resources"] . "</td><td>" . $row["Contact_info"] . "</td></tr>";
         }
 
         echo "</table>";
-    } else {
+    } 
+    else 
+    {
         echo "0 results";
     }
 
