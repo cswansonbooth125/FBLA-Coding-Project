4652defe8bf5a06cf386a3407ff147063d91e5b3
cswansonbooth125 <cswansonbooth125@gmail.com>
2024-02-05 00:24:15 -0700

    displays results of search on webpage

---
+++ display-search-script.php
@@ -0,0 +1,48 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "partners";
+
+try {
+    $conn = new mysqli($servername, $username, $password, $dbname);
+} catch (Exception $e) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM search_results";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Start of responsive-table container
+    echo "<div class='container'>";
+    echo "<h2>Partner Information</h2>";
+    echo "<ul class='responsive-table'>";
+    
+    // Table header
+    echo "<li class='table-header'>";
+    echo "<div class='col col-1'>Name</div>";
+    echo "<div class='col col-2'>Type</div>";
+    echo "<div class='col col-3'>Resources</div>";
+    echo "<div class='col col-4'>Contact Info</div>";
+    echo "</li>";
+
+    while ($row = $result->fetch_assoc()) {
+        // Table rows
+        echo "<li class='table-row'>";
+        echo "<div class='col col-1' data-label='Name'>" . $row["Name"] . "</div>";
+        echo "<div class='col col-2' data-label='Type'>" . $row["Type"] . "</div>";
+        echo "<div class='col col-3' data-label='Resources'>" . $row["Resources"] . "</div>";
+        echo "<div class='col col-4' data-label='Contact Info'>" . $row["Contact_info"] . "</div>";
+        echo "</li>";
+    }
+
+    // End of responsive-table container
+    echo "</ul>";
+    echo "</div>";
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+
---
+++ search-page.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@600&display=swap" rel="stylesheet">
+</head>
+<body class = 'index-body'>
+    <div class="main-header">
+        <h1>Our Partners</h1>
+    </div>
+
+    <form action="search-script.php" method="post">
+        <label for="searchTerm">Search:</label>
+        <input type="text" id="searchTerm" name="searchTerm" required>
+        <button type="submit">Search</button>
+    </form>
+
+    
+    <?php
+        include("display-search-script.php");
+    ?>
+
+    <form class = 'login-form' action="login-script.php" method="post">
+        <p>If you are an admin you can login below.</p>
+        <label for="Username"></label>
+        <input class = 'update-input' type="text" name="uname" placeholder="Username"><br>
+
+        <label for="Password"></label>
+        <input class = 'update-input' type="password" name="password" placeholder="Password"><br>
+
+        <button class = 'button-13' type="submit">Login</button>
+        <?php if (isset($_GET['error'])) { ?>
+            <p class="error"><?php echo $_GET['error']; ?></p>
+        <?php } ?>
+            
+        
+    </form>
+
+    
+    
+
+</body>
+</html>
